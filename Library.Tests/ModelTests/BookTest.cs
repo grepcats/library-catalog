3791f1c0e81c8b22591b169ee4b292d3537f4ee3
Kayla Ondracek <kayla.renee@gmail.com>
2018-02-28 09:36:04 -0800

    add methods to GetAll books, Save book, and an Equals override, including green tests for these

--- Library.Tests/ModelTests/BookTest.cs
+++ Library.Tests/ModelTests/BookTest.cs
@@ -6,8 +6,13 @@ using System;
 namespace Library.Tests
 {
   [TestClass]
-  public class BookTest
+  public class BookTest : IDisposable
   {
+    public void Dispose()
+    {
+      Book.DeleteAll();
+    }
+
     public BookTest()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=library_test;";
@@ -28,6 +33,58 @@ namespace Library.Tests
       //assert
       Assert.AreEqual(testTitle, resultTitle);
       Assert.AreEqual(testId, resultId);
+    }
+
+    [TestMethod]
+    public void GetAll_DatebaseEmptyAtFirst_0()
+    {
+      //arrange, act
+      int result = Book.GetAll().Count;
+
+      //assert
+      Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void Save_SaveBookToDatabase_BookList()
+    {
+      //arrange
+      Book testBook = new Book("Consider Phlebas");
+
+      //act
+      testBook.Save();
+      List<Book> result = Book.GetAll();
+      List<Book> testList = new List<Book>{testBook};
+
+      //assert
+      CollectionAssert.AreEqual(result, testList);
+    }
+
+    [TestMethod]
+    public void Save_AssignedIdToObject_Id()
+    {
+      //arrange
+      Book testBook = new Book("Consider Phlebas");
+
+      //act
+      testBook.Save();
+      Book savedBook = Book.GetAll()[0];
+      int result = savedBook.GetId();
+      int testId = testBook.GetId();
+
+      //assert
+      Assert.AreEqual(result, testId);
+    }
+
+    [TestMethod]
+    public void Equals_ReturnsTrueIfAllSame_Book()
+    {
+      //arrange, act
+      Book firstBook = new Book("Consider Phlebas");
+      Book secondBook = new Book("Consider Phlebas");
+
+      //assert
+      Assert.AreEqual(firstBook, secondBook);
     }
   }
 }
--- Library/Models/Books.cs
+++ Library/Models/Books.cs
@@ -19,5 +19,86 @@ namespace Library.Models
 
     public int GetId() { return _id; }
 
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM books;";
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if(conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO `books` (`title`) VALUES (@BookTitle);";
+
+      MySqlParameter bookTitle = new MySqlParameter();
+      bookTitle.ParameterName = "@BookTitle";
+      bookTitle.Value = this._title;
+      cmd.Parameters.Add(bookTitle);
+
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Book> GetAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM books";
+
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      List<Book> allBooks = new List<Book>{};
+      while(rdr.Read())
+      {
+        int bookId = rdr.GetInt32(0);
+        string bookTitle = rdr.GetString(1);
+        Book newBook = new Book(bookTitle, bookId);
+        allBooks.Add(newBook);
+      }
+
+      return allBooks;
+    }
+
+    public override bool Equals(System.Object otherBook)
+    {
+      if(!(otherBook is Book))
+      {
+        return false;
+      }
+      else
+      {
+        Book newBook = (Book) otherBook;
+        bool bookIdEquality = (this.GetId() == newBook.GetId());
+        bool bookTitleEquality = (this.GetTitle() == newBook.GetTitle());
+        return (bookIdEquality && bookTitleEquality);
+      }
+    }
+
+    public override int GetHashCode()
+     {
+          return this.GetTitle().GetHashCode();
+     }
+
   }
 }
